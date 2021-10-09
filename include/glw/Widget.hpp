d2a7208ac7b5344d0259ac0cb334cf9dc7de0abb
huangqinjin <huangqinjin@gmail.com>
2021-10-10 02:49:41 +0800

    add license

--- include/glw/Widget.hpp
+++ include/glw/Widget.hpp
@@ -1,7 +1,10 @@
 //
-// Created by huangqinjin on 6/23/17.
+// Copyright (c) 2017-2021 Huang Qinjin (huangqinjin@gmail.com)
 //
-
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+//
 #ifndef GLW_WIDGET_HPP
 #define GLW_WIDGET_HPP
 
